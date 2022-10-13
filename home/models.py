762aa38aa65bee754218c6df47ec5373b88cd5b3
Léon ZHANG <zhangleon1985@gmail.com>
2022-10-13 16:54:37 +0200

    ajouter home page et admin

--- home/models.py
+++ home/models.py
@@ -2,6 +2,51 @@ from django.db import models
 
 from wagtail.models import Page
 
+from wagtail.admin.edit_handlers import FieldPanel
+
+from streams import blocks
+
+from wagtail.core.fields import StreamField
+
 
 class HomePage(Page):
-    pass
+    lead_text = models.CharField(
+        max_length=140,
+        blank=True,
+        help_text = "Sous-titre sous la bannière"
+    )
+    button = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Sélectionner une page à linker",
+        on_delete=models.SET_NULL
+    )
+    button_text = models.CharField(
+        max_length=50,
+        default= "Read More",
+        blank=False,
+        help_text="Bouton pour le texte",
+    )
+    banner_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="bannière arrière plan",
+        on_delete = models.SET_NULL
+    )
+
+    body = StreamField(
+        [("title",
+         blocks.TitleBlock())], null=True, blank=True
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        FieldPanel("button"),
+        FieldPanel("button_text"),
+        FieldPanel("banner_background_image"),
+        FieldPanel("body")
+    ]
